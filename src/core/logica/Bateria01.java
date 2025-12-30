0ab5c4d04c5a084c1618c595dddecb76fa5db318
Ezequiel <ezequielsilvalima01@gmail.com>
2025-12-30 20:41:10 -0300

    commit

--- src/core/logica/Bateria01.java
+++ src/core/logica/Bateria01.java
@@ -3,6 +3,7 @@ package core.logica;
 public class Bateria01 {
     public static void main(String[] args) {
         exe01();
+        exe02();
     }
 
     public static void exe01(){
@@ -15,6 +16,20 @@ public class Bateria01 {
 
         double f = c * 9 / 5 + 32;
 
-        System.out.println(c + " graus Celsius equivale a " + f + " graus Fahrenheit");
+        System.out.printf("%.0f graus Celsius equivale a %.0f graus Fahrenheit", c, f);
+    }
+
+    public static void exe02(){
+        /*
+         b) Ler uma temperatura em graus Fahrenheit e apresentá-la
+        convertida em graus Celsius. A fórmula de conversão é C ←
+        ((F – 32) * 5) / 9, sendo F a temperatura em Fahrenheit e C
+        */
+
+        double f = 20;
+
+        double c = ((f - 32) * 5) / 9;
+
+        System.out.printf("\n%.0f graus Fahrenheit equivale a %.0f graus Celsius", f, c);
     }
 }
